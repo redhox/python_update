3d0bf653e00a3c23f02bd2efe4ded67110eba801
Votre Nom <Vous@exemple.com>
2024-09-14 16:31:19 +0200

    7

--- app.py
+++ app.py
@@ -1,30 +1,36 @@
 from git import Repo, GitCommandError
 
 # Chemin vers le répertoire git de ton projet
-repo_path = '.git'
+repo_path = '.'  # Utilise le répertoire racine du dépôt
 
-# Ouvrir le dépôt Git
-repo = Repo(repo_path)
+try:
+    # Ouvrir le dépôt Git
+    repo = Repo(repo_path)
 
-# Obtenir le commit actuel (HEAD) de la branche locale
-current_commit = repo.head.commit
+    # Obtenir le commit actuel (HEAD) de la branche locale
+    current_commit = repo.head.commit
 
-# Obtenir le commit de la branche `origin/main`
-origin_main_commit = repo.commit('origin/main')
+    # Effectuer un fetch pour récupérer les dernières données du serveur
+    repo.remotes.origin.fetch()
 
-# Comparer les deux commits
-if current_commit == origin_main_commit:
-    print("Le commit actuel est le même que celui de 'origin/main'.")
-else:
-    print("Le commit actuel est différent de celui de 'origin/main'.")
-    
-    try:
-        # Faire un fetch pour récupérer les dernières modifications
+    # Obtenir le commit de la branche 'origin/main'
+    origin_main_commit = repo.commit('origin/main')
+
+    # Comparer les deux commits
+    if current_commit == origin_main_commit:
+        print("Le commit actuel est le même que celui de 'origin/main'.")
+    else:
+        print("Le commit actuel est différent de celui de 'origin/main'.")
+        print("fetch du depot")
         repo.remotes.origin.fetch()
-        print("Fetch terminé avec succès.")
+        # Effectuer un pull pour mettre à jour la branche locale
+        print("Mise à jour du dépôt...")
 
-        # Faire un pull pour mettre à jour la branche locale
         repo.remotes.origin.pull()
-        print("Pull terminé avec succès.")
-    except GitCommandError as e:
-        print(f"Une erreur est survenue lors du fetch ou du pull : {e}")
+
+        print("Mise à jour terminée.")
+
+except GitCommandError as e:
+    print(f"Erreur lors de l'exécution des commandes Git : {e}")
+except Exception as e:
+    print(f"Erreur : {e}")
